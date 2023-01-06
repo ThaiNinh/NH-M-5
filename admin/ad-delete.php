ff3cdd40e0098bffbdd488e0c5b4702aefd33652
ThaiNinh <nimtn88@gmail.com>
2023-01-06 19:40:49 +0700

    second commit

---
+++ admin/ad-delete.php
@@ -0,0 +1,18 @@
+<?php
+include 'config.php';
+$_SESSION['name'];
+$_SESSION['type'];
+if($_SESSION['type'] == 1){
+    echo 'Không làm admin đòi vào à?';
+    die();
+}
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+
+    $sql ="DELETE FROM `tbl_loaisp` WHERE loaisp_id =$id";
+    $query=mysqli_query($conn, $sql);
+    if($query){
+        header('location: admin.php');
+    }
+}
+?>
---
+++ admin/ad-update.php
@@ -0,0 +1,64 @@
+<?php
+include 'config.php';
+$_SESSION['name'];
+$_SESSION['type'];
+if($_SESSION['type'] == 1){
+    echo 'Không làm admin đòi vào à?';
+    die();
+}
+$id = $_GET['id'];
+if(isset($_POST['tenloaisp'])){
+
+    $tenloaisp = $_POST['tenloaisp'];
+    $sql = "UPDATE `tbl_loaisp` SET `tenloaisp`='$tenloaisp' WHERE loaisp_id='$id'";
+    $query = mysqli_query($conn, $sql);
+    if($query){
+    header("location: admin.php");
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>DASHMIN - Bootstrap Admin Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;700&display=swap" rel="stylesheet">
+    
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container-fluid pt-4 px-4">
+        <form method="post">
+            <div class="bg-light text-center rounded p-4">
+                <input type="text" class="bg-light text-center rounded p-4" placeholder="Tên Loại Sản Phẩm" name="tenloaisp">
+                <button type="submit" class="btn btn-primary ms-2 mr-2" id="submitBtn">Sửa</button></td>
+            </div>
+        </form>
+    </div>
+</body>
