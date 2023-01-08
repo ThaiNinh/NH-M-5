5391bb11be56bdbd5edbd13fd05085f1da1fd898
ThaiNinh <nimtn88@gmail.com>
2023-01-09 01:25:09 +0700

    del

--- database/dbcon.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	$conn = new mysqli('localhost', 'root', '', 'xustorem');
-	if(!$conn){
-		die("Fatal Error: Connection Error!");
-	}
-	mysqli_query($conn,'set names utf8');
-?>
