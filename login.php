7b79d4248465577001745d87a8a751ff0d6ccd2f
Lanh2307 <you@example.com>
2023-01-11 15:54:21 +0700

    doi dnhapdki

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-	require("header.php");
+	include 'config.php';
 
 if(isset($_POST['name'])){
     $name= $_POST['name'];
@@ -21,6 +21,119 @@ if(isset($_POST['name'])){
         }
     }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xu Store</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel = "stylesheet" type = "text/css" href="eshopper-1.0.0/css/style.css" media="all">
+	<link rel = "stylesheet" href="fontawesome-free-6.2.1-web/css/all.min.css">
+	<link rel = "stylesheet" href="fontawesome-free-6.2.1-web/css/all.css">
+	<link rel = "stylesheet" href="./stylecss.css">
+	<script src="eshopper-1.0.0/js/main.js"></script>
+    <style>
+            .has-error{
+                color: red;
+            }
+        </style>
+
+</head>
+
+<body>
+    <!-- Topbar Start -->
+
+	<header class="row py-2 px-5 justify-content-between">
+		<div class="col d-sm-none d-lg-block">
+			<a class="font-monospace font-weight-light " style="color:black; font-size:3rem; text-decoration: none; letter-spacing: 5px;">XU STORE</a>		
+		</div>
+		<div class="col text-right">
+        <a href="login.php" class="d-none d-lg-inline-flex page-item text"style="text-decoration: none">
+		Đăng Nhập 
+		<span>&#160;<i class="fa-solid fa-right-to-bracket"></i>&ensp;</span> </a>
+		<a href="register.php" class="nd-none d-lg-inline-flex page-item text"style="text-decoration: none">
+		Đăng Ký
+		<span>&#160;<i class="fa-regular fa-registered"></i></span> </a>		
+		</div>	
+    
+	<nav class="row justify-content-between" style="width:100%">
+			<nav class="menu navbar navbar-expand-lg py-3 py-lg-0 px-0">
+				<div class="collapse navbar-collapse ">
+					<ul class="navbar-nav me-auto col-7">			  
+					  <li class="nav-item">
+						<a class="nav-link active " aria-current="page" href="#">
+							<i class="fas fa-home"></i>
+							Trang chủ<span class="ui-button-icon-only"></span>
+						</a>				
+					  </li>
+					  
+					  <li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="gioithieu.html">
+							<i class="fa-solid fa-users-between-lines"></i>
+							Về chúng tôi<span class="ui-button-icon-only"></span>
+						</a>				
+					  </li>
+					  
+					  <li class="nav-item dropdown">
+					    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+					    <i class="fa-solid fa-bars"></i>
+						Sản phẩm<span class="ui-button-icon-only"></span>
+					    </a>
+						<div class="dropdown-menu rounded-0 m-0">
+							<a href="shop.php?loaisp_id=1" class="dropdown-item">Giày nam</a>
+							<a href="shop.php?loaisp_id=2" class="dropdown-item">Giày nữ</a>
+							<a href="shop.php?loaisp_id=3" class="dropdown-item">Giày trẻ em</a>
+						</div>
+					  </li>
+
+					  <li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#">
+							<i class="fa-regular fa-newspaper"></i>
+							Tin tức<span class="ui-button-icon-only"></span>
+						</a>				
+					  </li>
+					  <li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="lienhe.php">
+							<i class="fa-solid fa-phone"></i>
+							Liên hệ<span class="ui-button-icon-only"></span>
+						</a>				
+					  </li>
+					  <li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="lichsumuahang.php">
+							<i class="fa-solid fa-shopping-cart"></i>
+							Lịch sử mua hàng<span class="ui-button-icon-only"></span>
+						</a>				
+					  </li>
+
+					</ul>
+					<div class="col-lg-4 col-6 text-left">
+                        <div class="input-group">
+                	    <form name="sform" action="timkiem.php?quanly=timkiem" method="POST">
+                        <input type="text" class="form-control" placeholder="Tìm kiếm sản phẩm..." name="tukhoa">
+                        <div class="input-group-append">
+							
+							<button class="input-group-text bg-transparent text-primary" name="timkiem" type="submit" value="tìm kiếm">
+                                <i class="fa fa-search"></i>
+                            </button>
+							
+                        </div>                  
+                        </form>
+				        </div>
+					</div>
+					<div class="col-2">			
+						<a href="" class="btn border">
+							<i class="fas fa-shopping-cart text-primary"></i>
+							<span class="badge">0</span>
+						</a>
+					</div>
+				</div>	
+			</nav>
+		</nav>
+	</header>
+
 
 <!-- Page Header Start -->
 <section class="container-fluid bg-secondary">
---
+++ thanhtoan.php
@@ -0,0 +1,11 @@
+<?php
+	require("header.php");
+?>
+<?php
+    $id_nguoidung=$_SESSION['nguoidung_id'];
+    $sanpham_id=$_SESSION['sanpham_id'];
+    $soluong=$_SESSION['so_luong_cart'];
+    $size=$_SESSION['so_size'];
+    $insert_giohang="INSERT INTO `tbl_giohang`( `sanpham_id`, `so_luong_cart`, `nguoidung_id`, `size`) 
+    VALUES ('".$sanpham_id."','".$soluong."','".$id_nguoidung"','".$size."')";
+    $cart
